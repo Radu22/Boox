c1941e73b3e4d0b15d0c64a212506aae8060ef5f
Maftei Radu <mafteiradu23@yahoo.com>
2018-03-18 00:24:22 +0200

    finished social media buttons

--- Register/register_footer.php
+++ Register/register_footer.php
@@ -2,7 +2,12 @@
 
 echo '		<div class="footer">
 					<h4> You can find us on </h4>
+					<ul>
+						<li><a href="#" class="fa fa-facebook"> </a></li>
+						<li><a href="#" class="fa fa-google"> </a></li>
+						<li><a href="#" class="fa fa-twitter"> </a></li>
 
+					</ul>
 
 
 			</div>';
