dce36e1e3c57675546f16bb857e93c6fb22336c7
Maftei Radu <mafteiradu23@yahoo.com>
2018-03-20 16:29:44 +0200

    restructure3

---
+++ Headers/header_main.php
@@ -0,0 +1,18 @@
+
+<?php
+
+echo '	<div class="header" id="myHeader">
+			<a href="#" class="logo">Logo</a>
+			<div class="header-right">
+
+			<a href="#" class="active">Homepage </a>
+			<a href="#">My Profile</a>
+			<a href="#">My books</a>
+			<a href="#">Community</a>
+			<a href="#">Contact us</a>
+
+			</div>
+
+		</div>';
+
+?>
