694248c3365977fc9bbb19b96ada450a9add2696
Maftei Radu <mafteiradu23@yahoo.com>
2018-03-20 16:16:29 +0200

    restructure

--- Profile/profile.php
+++ Profile/profile.php
@@ -10,21 +10,8 @@
 
     </head>
     <body>
-		<div class="header" id="myHeader">
-			<a href="#" class="logo">Logo</a>
-			<div class="header-right">
 
-			<a href="#" class="active">Homepage </a>
-			<a href="#">My Profile</a>
-			<a href="#">My books</a>
-			<a href="#">Community</a>
-			<a href="#">Contact us</a>
-
-			</div>
-
-		</div>
-
-
+		<?php include "../Headers/header_main.php";?>
 		<div class="content">
 			<p>Some text I wrote myself</p>
 			<p>Some text I wrote myself</p>
