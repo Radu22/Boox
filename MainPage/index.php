dfe2aeea4bf917404fd5bf90d14bb63b635bd11a
Bernard Pirlea <pirlea.bernard@gmail.com>
2018-03-20 16:21:46 +0200

    homepage

---
+++ MainPage/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<link rel="stylesheet"  href="mainpage.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	</head>
+<body>
+
+<div class="header">
+  <h1>Aici trebuie headerul comun</h1>
+</div>
+
+<div class="topnav">
+  <a href="#">Link</a>
+  <a href="#">Link</a>
+  <a href="#">Link</a>
+  <a href="#" style="float:right">Link</a>
+</div>
+
+<div class="row">
+  <div class="leftcolumn">
+    <div class="card">
+      <h2>Titlu Carte</h2>
+      <h5>Autor</h5>
+      <div class="fakeimg" style="height:200px;">Image</div>
+    </div>
+    <div class="card">
+      <h2>Titlu Carte</h2>
+      <h5>Autor</h5>
+      <div class="fakeimg" style="height:200px;">Image</div>
+    </div>
+  </div>
+  <div class="rightcolumn">
+  	<div class="searchButton">
+	  	<form class="example" action="/action_page.php" style="margin:auto;max-width:500px">
+			<input type="text" placeholder="Cauta.." name="search2">
+			<button type="submit"><i class="fa fa-search"></i></button>
+		</form>
+	</div>
+    <div class="addButton">
+    	<button class="button">Adauga Carte</button>
+  </div>
+
+</div>
+
+</body>
+</html>
