7a237eed936ffc0708a7cd8444a9237dbab3a4ee
Bernard Pirlea <pirlea.bernard@gmail.com>
2018-03-21 00:20:47 +0200

    Header

--- AddBook/AddBook.php
+++ AddBook/AddBook.php
@@ -1,9 +1,16 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-</head>
-<body>
+	<head>
+		<title>Adauga Carte</title>
+		<meta charset="utf-8">
+	    <link rel="stylesheet" href="addbook.css">
+	    <link rel="stylesheet" href="../css/footer.css">
+        <link rel="stylesheet" href="../css/header.css">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body>
 
-</body>
+		 <?php include "../Headers/header_main.php";?>
+
+	</body>
 </html>
