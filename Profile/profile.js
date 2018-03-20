213900b5385ea858328b6cdbe7fc48f42240d2f5
Maftei Radu <mafteiradu23@yahoo.com>
2018-03-20 11:33:09 +0200

    created sticky header

---
+++ Profile/profile.js
@@ -0,0 +1,15 @@
+// Makes header sticky on scroll
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+// -- end
---
+++ Profile/profile.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html>
+    <head>
+		<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>My Profile</title>
+
+        <link rel="stylesheet" href="../css/footer.css">
+        <link rel="stylesheet" href="../css/header.css">
+
+    </head>
+    <body>
+		<div class="header" id="myHeader">
+			<ul>
+				<li>Homepage</li>
+				<li>My Profile</li>
+				<li>My books</li>
+				<li>Community</li>
+				<li>Contact us</li>
+			</ul>
+
+		</div>
+
+
+		<div class="content">
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+			<p>Some text I wrote myself</p>
+		</div>
+
+
+
+
+
+
+
+
+		<script src="profile.js"></script>
+
+    </body>
+</html>
