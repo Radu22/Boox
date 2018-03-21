f5950ed86d3bf8bc352ac17e2e49fb3ae0f348a7
Maftei Radu <mafteiradu23@yahoo.com>
2018-03-22 00:59:09 +0200

    deleted garbage

--- Headers/header_main.php
+++ Headers/header_main.php
@@ -7,7 +7,6 @@ echo '	<div class="header" id="myHeader">
 
 			<a href="../MainPage/index.php" class="active" >Homepage </a>
 			<a href="../Profile/books.php">My books</a>
-			<a href="../Community/index.php" >Friends</a>
 			<a href="../Contact/index.php">Contact us</a>
 			<a href="../Register/index.php">Logout</a>
 			<a href="../Profile/profile.php" class="fa fa-user-circle" id="myProf"
