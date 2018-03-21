0850a2de5c468a4a2954cb2b0750c5c9f2e7bed8
Maftei Radu <mafteiradu23@yahoo.com>
2018-03-21 20:07:41 +0200

    added pages for header links.

---
+++ Profile/books.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+    <head>
+		<meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Personal books</title>
+
+        <link rel="stylesheet" href="../css/footer.css">
+        <link rel="stylesheet" href="../css/header.css">
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    </head>
+    <body>
+
+		<?php include "../Headers/header_main.php";?>
+
+	<div class="content">
+
+
+	</div>
+
+
+
+
+		<script src="profile.js"></script>
+
+    </body>
+</html>
