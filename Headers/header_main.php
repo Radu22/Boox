cb0151b7d831567c640e2a204b2eb2972a258f7e
Maftei Radu <mafteiradu23@yahoo.com>
2018-03-21 22:56:54 +0200

    tabs bar work

--- Headers/header_main.php
+++ Headers/header_main.php
@@ -5,10 +5,11 @@ echo '	<div class="header" id="myHeader">
 			<a href="#" class="logo">Logo</a>
 			<div class="header-right">
 
-			<a href="../MainPage/index.php" class="active">Homepage </a>
-			<a href="../Profile/books.php" class="secondary">My books</a>
-			<a href="../Community/index.php" class="secondary">Friends</a>
-			<a href="../Contact/index.php" class="secondary">Contact us</a>
+			<a href="../MainPage/index.php" class="active" >Homepage </a>
+			<a href="../Profile/books.php">My books</a>
+			<a href="../Community/index.php" >Friends</a>
+			<a href="../Contact/index.php">Contact us</a>
+			<a href="../Register/index.php">Logout</a>
 			<a href="../Profile/profile.php" class="fa fa-user-circle" id="myProf"></a>
 
 		  </div>
--- Profile/profile.php
+++ Profile/profile.php
@@ -20,13 +20,14 @@
 		<h2>Hello, $name</h2>
 				<div id="myTabs">
 					<ul>
-						<li class="fa fa-pencil-square-o">Edit</li>
-						<li class="fa fa-gears">Settings</li>
-						<li class="fa fa-thumbs-o-up">Following</li>
-						<li class="fa fa-heart">Followers</li>
+						<li class="fa fa-pencil-square-o under" ><a href="#edit">Edit</a></li>
+						<li class="fa fa-gears under" ><a href="#settings">Settings</a></li>
+						<li class="fa fa-thumbs-o-up under"><a href="#follow">Following</a></li>
+						<li class="fa fa-heart under" ><a href="#followed">Followers</a></li>
 					</ul>
-
 				</div>
+			<p id="edit">Lorem Ipsum este pur şi simplu o machetă pentru text a industriei tipografice. Lorem Ipsum a fost macheta standard a industriei încă din secolul al XVI-lea, când un tipograf anonim a luat o planşetă de litere şi le-a amestecat pentru a crea o carte demonstrativă pentru literele respective. Nu doar că a supravieţuit timp de cinci secole, dar şi a facut saltul în tipografia electronică practic neschimbată. A fost popularizată în anii '60 odată cu ieşirea colilor Letraset care conţineau pasaje Lorem Ipsum, iar mai recent, prin programele de publicare pentru calculator,</p>
+
 
 		</div>
 
