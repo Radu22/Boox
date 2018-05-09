2b1acb94a33155f6a60eaac7129f43d922a4f971
Maftei Radu <mafteiradu23@yahoo.com>
2018-05-09 23:19:45 +0300

    database diagram added

--- Architecture/DB/db.sql
+++ Architecture/DB/db.sql
@@ -1,4 +1,8 @@
-
+drop table USERS;
+drop table book_added;
+drop table book_wanted;
+drop table user_group;
+/
 
 CREATE TABLE USERS (
 	user_id 		 INT NOT NULL PRIMARY KEY,
@@ -9,7 +13,7 @@ CREATE TABLE USERS (
 	password 		 VARCHAR2(20) NOT NULL
 );
 
-CREATE TABLE BOOK_ADDED(
+CREATE TABLE BOOK_ADDED (
    book_id 			INT NOT NULL PRIMARY KEY,
    id 				INT NOT NULL,
    book_title		VARCHAR2(50) NOT NULL,
@@ -29,6 +33,17 @@ CREATE TABLE BOOK_WANTED(
    description 		VARCHAR2(200),
 
    CONSTRAINT fk_want FOREIGN KEY (id) REFERENCES USERS(user_id)
+);
+
+CREATE TABLE user_group(
+	id 				INT NOT NULL PRIMARY KEY,
+	id_first		INT NOT NULL,
+	id_second 		INT NOT NULL,
+
+	CONSTRAINT fk_id_first  FOREIGN KEY (id_first)  REFERENCES USERS(user_id),
+	CONSTRAINT fk_id_second FOREIGN KEY (id_second) REFERENCES USERS(user_id),
+
+	CONSTRAINT no_dups UNIQUE (id_first,id_second)
 );
 
 CREATE TABLE IMAGE(
