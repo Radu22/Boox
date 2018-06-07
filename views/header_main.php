e06db75a7e43e92ceabe71b15554b0ae4beb38d8
Maftei Radu <mafteiradu23@yahoo.com>
2018-06-07 18:21:55 +0300

    front notif done

--- views/header_main.php
+++ views/header_main.php
@@ -3,14 +3,25 @@
 
 echo '	<div class="header" id="myHeader">
 			<a href="#" class="logo">Logo</a>
+
+			<div class="header-left">
+
+				<a class="fa fa-bell">
+					<div class="notification">30</div>
+				</a>
+				
+
+			</div>
+
+
 			<div class="header-right">
 
-			<a href="main.php?controller=pages&action=main" class="active" >Homepage </a>
-			<a href="book.php?controller=pages&action=book">My books</a>
-			<a href="contact.php?controller=pages&action=contact">Contact us</a>
-			<a href="logout.php">Logout</a>
-			<a href="profile.php?controller=pages&action=profile" class="fa fa-user-circle" id="myProf"
-					style="text-decoration:none;"></a>
+				<a href="main.php?controller=pages&action=main" class="active" >Homepage </a>
+				<a href="book.php?controller=pages&action=book">My books</a>
+				<a href="contact.php?controller=pages&action=contact">Contact us</a>
+				<a href="logout.php">Logout</a>
+				<a href="profile.php?controller=pages&action=profile" class="fa fa-user-circle" id="myProf"
+						style="text-decoration:none;"></a>
 
 		  </div>
 
