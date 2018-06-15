75b0cccd6bf2e5435ab6d24fbfcd07f905c65486
Bernard <pirlea.bernard@gmail.com>
2018-06-15 11:58:20 +0300

    trade new

--- controllers/pages_controller.php
+++ controllers/pages_controller.php
@@ -147,7 +147,7 @@
     }
     public function notification(){
       if(!empty($_POST['titlu_carte']) && !empty($_POST['user_to'])){
-        Notification::insertNotification($_POST['titlu_carte'], $_POST['user_to']);
+        Notification::insertNotification($_POST['user_to'],$_POST['titlu_carte'] );
         header("Location: ../pages/notification.php?controller=pages&action=notification");
         // requice_once("../pages/notification.php?controller=pages&action=notification");
       }else{
@@ -157,7 +157,7 @@
       // if(!empty($_POST['titlu_carte']) && !empty($_POST['user_to'])){
       //     var_dump($_POST['titlu_carte']);
       //     var_dump($_POST['user_to']);
-      
+
       // }
 
     }
