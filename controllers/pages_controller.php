e71facbff78a0b7d156b2089196a67c174787747
Maftei Radu <mafteiradu23@yahoo.com>
2018-06-18 14:11:58 +0300

    fixed notification

--- controllers/pages_controller.php
+++ controllers/pages_controller.php
@@ -150,12 +150,85 @@
 
     }
     public function notification(){
-      if(!empty($_POST['titlu_carte']) && !empty($_POST['user_to'])){
-        Notification::insertNotification($_POST['user_to'],$_POST['titlu_carte'] );
+    $db = Db::getInstance();
+    $req = $db->prepare('SELECT * FROM notification WHERE user_to = :id and seen = :val_seen' );
+    $req->bindValue(":id", $_SESSION['id'] );
+    $req->bindValue(":val_seen", 1 );
+	  $req->execute();
+    echo '<form action="../pages/notification.php?controller=pages&action=notification" method="post">';
+    
+    $count = 0;
+    foreach($req->fetchAll() as $notif){
+    	$id = $notif['user_from'];
+    	$sql = $db->prepare('SELECT * FROM book_added WHERE user_id = :id');
+    	$sql->bindValue(":id", $id);
+		  $sql->execute();
+
+        if(substr($notif['type'],0,5)=="trade"){
+            foreach($sql->fetchAll() as $book){
+                $ql = $db->prepare('SELECT user_first FROM users WHERE user_id = :id');
+                $ql->bindValue(":id", $book['user_id']);
+                $ql->execute();
+                $result = $ql->fetch();
+                $nume = $result['user_first'];
+                echo "<div class= card>";
+                    echo "<h3>" . $nume . " wants to trade <b>". $book['book_title'] ."</b> for</h3>";
+                    echo '<p>'. substr($notif['type'],5) ." </p> ";
+
+                echo "</div>";
+        }
+
+        }else{
+            foreach($sql->fetchAll() as $book){
+
+            $ql = $db->prepare('SELECT user_first FROM users WHERE user_id = :id');
+            $ql->bindValue(":id", $book['user_id']);
+            $ql->execute();
+            $result = $ql->fetch();
+            $nume = $result['user_first'];
+
+
+            echo "<div class= card>";
+                echo "<h4><b>" . $nume . " is less then 5 km away and has this for trade:</b></h4> ";
+                echo "<p>Titlu: ". $book['book_title'] . "</p>";
+                echo "<p>ISBN: ". $book['ISBN'] . "</p>";
+                echo "<p>Limba: ". $book['language'] . "</p>";
+
+                $count++;
+                echo '<input type="text" name="user_to' . $count . '" class="titlu" value=' . $id . '>';
+                echo '<input type="text" name="titlu_carte' . $count . '" class="titlu" value=' . $book['book_title'] . '>';
+                if(Book::getCount('book_added') == 0){
+                    echo '<p> You don\'t have any books to trade</p>';
+                }else
+                {
+                    echo '<input type="submit" name="trade" value="trade' . $count .  '" style="font-size:20px;">';
+                }
+
+            echo "</div>";
+        }
+      }
+
+
+
+	}
+	echo '</form>';
+
+
+      if($_SERVER['REQUEST_METHOD'] === 'POST'){
+  
+        $number = substr($_POST['trade'], -1);
+        if(!empty($number)){
+            $title = 'titlu_carte' . $number; 
+            $userish = 'user_to' . $number;
+        }
+        
+      if(!empty($_POST[$title]) && !empty($_POST[$userish])){
+        Notification::insertNotification($_POST[$userish], $_POST[$title] );
         header("Location: ../pages/notification.php?controller=pages&action=notification");
       }
      
     }
+}
 
 
     public function logout(){
--- views/pages/notification.php
+++ views/pages/notification.php
@@ -1,17 +1,7 @@
 
 <?php
   session_start();
-
-          if (isset($_GET['controller']) && isset($_GET['action'])) {
-          $controller = $_GET['controller'];
-          $action     = $_GET['action'];
-        } else {
-          $controller = 'pages';
-          $action     = 'reg';
-        }
-
-
-        require_once("../../routes.php");
+  
 ?>
 
 
@@ -34,5 +24,20 @@
 
        <div id="txtHint"></div>
 
+
+<?php 
+  if (isset($_GET['controller']) && isset($_GET['action'])) {
+    $controller = $_GET['controller'];
+    $action     = $_GET['action'];
+  } else {
+    $controller = 'pages';
+    $action     = 'reg';
+  }
+
+
+  require_once("../../routes.php");
+
+
+?>
       </body>
 </html>
